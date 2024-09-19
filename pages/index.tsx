cf646b67cbff39c31742036bdbd815758b133655
andsu <andsu.marks.r@gmail.com>
2024-09-19 11:38:55 -0300

    backup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
+import styles from "../styles/Formulario.module.css"
+import Cartao from "../components/Cartao";
+import Link from "next/link";
 import { useState } from "react";
-import Porta from "../components/Porta";
-import PortaModel from "../model/porta";
-import { atualizarPortas, criarPortas } from "../functions/portas";
-// import Presente from "@/components/Presente";
+import EntradaNumerica from "../components/EntradaNumerica";
 
-export default function Home() {
+export default function Formulario() {
+  const [qtdePortas, setQtdePortas] = useState(3);
+
   return (
-   <div style={{display:"flex"}}>
-    <h1>Formulario de Inicio!!!</h1>
+   <div className={styles.formulario}>
+    <div>
+      <Cartao bgcolor="#c0392c">
+        <h1 style={{backgroundColor: "#c0392c"}}>Monty Hall</h1>
+      </Cartao>
+      <Cartao>
+        <EntradaNumerica text="Qtde Portas" 
+          value={qtdePortas}
+          onChange={novaQtde => setQtdePortas(novaQtde)}/>
+      </Cartao>
+    </div>
+    <div>
+      <Cartao></Cartao>
+      <Cartao bgcolor="#28a085">
+        <Link href={`/jogo/4/2`}>
+          <h1 className={styles.link}>Iniciar</h1>
+        </Link>
+      </Cartao>
+    </div>
    </div>
   )
 }
